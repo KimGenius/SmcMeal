1fc2cc6dba8c13d3027ec61bf04e7513a1c20da9
KSH-Code <tjdgnsqn133@gmail.com>
2017-12-26 23:46:08 +0900

    function 분리

--- app.js
+++ app.js
@@ -1,109 +1,113 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+/* global $ */
+var express = require('express')
+var path = require('path')
+// var favicon = require('serve-favicon')
+// var logger = require('morgan')
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-var bab = require('./routes/bab');
-var login = require('./routes/login');
+var index = require('./routes/index')
+var users = require('./routes/users')
+var bab = require('./routes/bab')
+var login = require('./routes/login')
 
-var app = express();
+var app = express()
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 // app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
-app.use('/node-schedule', express.static(__dirname + '/node_modules/node-schedule'));
-app.use('/fb', express.static(__dirname + '/node_modules/fb/lib/'));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+app.use('/jquery', express.static(`${__dirname}/node_modules/jquery/dist/`))
+app.use('/node-schedule', express.static(`${__dirname}/node_modules/node-schedule`))
+app.use('/fb', express.static(`${__dirname}/node_modules/fb/lib/`))
 
-app.use('/', index);
-app.use('/users', users);
-app.use('/bab', bab);
-app.use('/login', login);
+app.use('/', index)
+app.use('/users', users)
+app.use('/bab', bab)
+app.use('/login', login)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
 
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  res.status(err.status || 500)
+  res.render('error')
+})
 
-const request = require('request');
-const cheerio = require('cheerio');
-const date = require('date-and-time');
-const schedule = require("node-schedule");
-const Entities = require('html-entities').XmlEntities;
-const entities = new Entities();
+const request = require('request')
+const cheerio = require('cheerio')
+const date = require('date-and-time')
+const schedule = require('node-schedule')
+const Entities = require('html-entities').XmlEntities
+const entities = new Entities()
 const page = require('./ignore/token')
-const FB = require('fb');
+const FB = require('fb')
 const info = require('./ignore/info')
 
 module.exports = app.listen(8160, function () {
   schedule.scheduleJob('25 * * * * *', function () {
     console.log(page.getToken())
     FB.setAccessToken(page.getToken())
-    const now = new Date();
-    const day = date.format(now, 'DD');
-    request(info.getUrl(), function (error, response, body) {
-      if (error) throw error;
-      var $ = cheerio.load(body);
-      var elements = $("tbody td div");
-      elements.each(function () {
-        var isToday = $(this).html().substr(0, 2);
-        if (isToday === day) {
-          var result = date.format(now, 'YYYY-MM-DD-dddd') + "\n"
-          var htmlData = $(this).html().split('<br>')
-          htmlData.forEach(function (data) {
-            if (data !== day) {
-              const encodeData = entities.decode(data).replace(/[0-9]|\./gi, '')
-              if (encodeData === "[중식]" || encodeData === "[석식]") {
-                result += "\n" + encodeData + "\n"
-              } else
-                result += encodeData + "\n"
-            }
-          })
-          if (result !== date.format(now, 'YYYY-MM-DD-dddd') + "\n") {
-            FB.api(
-              '/' + page.getPageId() + '/feed',
-              'POST',
-              { "message": result + "\n\nCreated by GeniusK & Leesane" + Math.random()},
-              function (res) {
-                if (!res || res.error) {
-                  console.log("feed err : ", !res ? 'error occurred' : res.error);
-                  return;
-                }
-                console.log("facebook board : ", res)
-                console.log('Post Id: ' + res.id);
-              }
-            );
-          } else {
-            console.log("주말 잘 보내세용")
-          }
-        }
-      });
-    });
-  });
+    request(info.getUrl(), function1)
+  })
   console.log('Bab is hot!')
-});
+})
+
+function function1 (err, response, body) {
+  if (err) throw err
+  global.$ = cheerio.load(body)
+  var elements = $('tbody td div')
+  elements.each(function2)
+}
+
+function function2 () {
+  const now = new Date()
+  const day = date.format(now, 'DD')
+  const isToday = $(this).html().substr(0, 2)
+  if (isToday === day) {
+    var result = date.format(now, 'YYYY-MM-DD-dddd') + '\n'
+    var htmlData = $(this).html().split('<br>')
+    for (let data of htmlData) {
+      if (data !== day) {
+        const encodeData = entities.decode(data).replace(/[0-9]|\./gi, '')
+        if (encodeData === '[중식]' || encodeData === '[석식]') {
+          result += '\n' + encodeData + '\n'
+        } else { result += encodeData + '\n' }
+      }
+    }
+    if (result !== date.format(now, 'YYYY-MM-DD-dddd') + '\n') {
+      FB.api(
+        '/' + page.getPageId() + '/feed',
+        'POST',
+        { 'message': result + '\n\nCreated by GeniusK & Leesane' + Math.random() },
+        function (res) {
+          if (!res || res.error) {
+            console.log('feed err : ', !res ? 'error occurred' : res.error)
+            return
+          }
+          console.log('facebook board : ', res)
+          console.log('Post Id: ' + res.id)
+        }
+      )
+    } else {
+      console.log('주말 잘 보내세용')
+    }
+  }
+}
